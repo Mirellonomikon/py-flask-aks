d09678c148bbb5af4bc62849bbcfe8a8e0e39896
Mirela Calinescu <mirelacalinescu2000@yahoo.com>
2023-08-23 16:06:15 +0300

    K

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -16,9 +16,9 @@ terraform {
 }
 
 provider "azurerm" {
-  client_id       = "6e2a0910-ab40-4d3b-94df-4c0a63f9cbbb"
-  client_secret   = "WUF8Q~2Uc-WNSmtSU3xEekeglZuTrsO~wmgc5c.B"
-  subscription_id = "9f6e1b5b-ecf2-47b0-9860-a5e1941bf68a"
-  tenant_id       = "a6eb79fa-c4a9-4cce-818d-b85274d15305"
+  # client_id       = "6e2a0910-ab40-4d3b-94df-4c0a63f9cbbb"
+  # client_secret   = "WUF8Q~2Uc-WNSmtSU3xEekeglZuTrsO~wmgc5c.B"
+  # subscription_id = "9f6e1b5b-ecf2-47b0-9860-a5e1941bf68a"
+  # tenant_id       = "a6eb79fa-c4a9-4cce-818d-b85274d15305"
   features {}
 }
