a000bafef3e047d1374ba70e70064efb9e579673
Mirela Calinescu <mirelacalinescu2000@yahoo.com>
2023-09-09 16:33:10 +0300

    nuj

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,8 +63,8 @@ resource "azurerm_role_assignment" "aksrole" {
   principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
 
-resource "azurerm_role_assignment" "agentpoolrole" {
-  scope = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id = azurerm_resource_group.aks.node_resource_group.kubelet_identity[0].object_id
-}
+# resource "azurerm_role_assignment" "agentpoolrole" {
+#   scope = azurerm_container_registry.acr.id
+#   role_definition_name = "AcrPull"
+#   principal_id = azurerm_resource_group.aks.node_resource_group.kubelet_identity[0].object_id
+# }
