aef9727a27b6f4f2f4483d57993d71fe46925317
Mirela Calinescu <mirelacalinescu2000@yahoo.com>
2023-09-10 21:55:29 +0300

    incercam

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,5 +47,5 @@ resource "azurerm_role_assignment" "aksrole" {
 resource "azurerm_role_assignment" "agentpoolrole" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.node_resource_group.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.node_resource_group_managed_identity[0].principal_id
 }
