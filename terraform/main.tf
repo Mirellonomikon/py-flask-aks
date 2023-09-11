4ae458f06fa374d2db1a3267ff93da13b1ac9deb
Mirela Calinescu <mirelacalinescu2000@yahoo.com>
2023-09-11 23:48:39 +0300

    aaaaaa

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # give acrpull role to kubernetes service (managed identity) for the container registry
 resource "azurerm_role_assignment" "acr_role" {
-  scope                = azurerm_container_registry.acr.id
+  scope                = azurerm_container_registry.acr.object_id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
@@ -52,7 +52,7 @@ data "azurerm_user_assigned_identity" "aks_node_identity" {
 
 # give acrpull role to aks-cluster-agentpool (managed identity) for the container registry
 resource "azurerm_role_assignment" "acragentpool_role" {
-  scope                = azurerm_container_registry.acr.id
+  scope                = azurerm_container_registry.acr.object_id
   role_definition_name = "AcrPull"
   principal_id         = data.azurerm_user_assigned_identity.aks_node_identity.id
 }
