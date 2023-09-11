35f5cff20918e2e94b2b47586915fe345a5b0f9f
Mirela Calinescu <mirelacalinescu2000@yahoo.com>
2023-09-12 02:17:17 +0300

    doamne da sa mearga

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,10 +40,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # give acrpull role to kubernetes service (managed identity) for the container registry
 resource "azurerm_role_assignment" "acr_role" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.identity[0].principal_id
-  skip_service_principal_aad_check = true
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
 }
 
 data "azurerm_user_assigned_identity" "aks_node_identity" {
@@ -53,8 +52,7 @@ data "azurerm_user_assigned_identity" "aks_node_identity" {
 
 # give acrpull role to aks-cluster-agentpool (managed identity) for the container registry
 resource "azurerm_role_assignment" "acragentpool_role" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = "AcrPull"
-  principal_id                     = data.azurerm_user_assigned_identity.aks_node_identity.principal_id
-  skip_service_principal_aad_check = true
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = data.azurerm_user_assigned_identity.aks_node_identity.principal_id
 }
