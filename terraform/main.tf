a4b0333ffd75560254624d0c7a6a507ca1db9f60
Mirela Calinescu <mirelacalinescu2000@yahoo.com>
2023-09-11 12:28:29 +0300

    create resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-resource "azurerm_role_assignment" "aksrole" {
+# give acrpull role to kubernetes service (managed identity) for the container registry
+resource "azurerm_role_assignment" "acr_role" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
